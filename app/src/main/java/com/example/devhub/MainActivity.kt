509ee43b57dcc732ef0861b1c208aa99eb4ab3b9
Ageu <acasil77@gmail.com>
2025-02-27 13:37:39 -0400

    1st step

--- app/src/main/java/com/example/devhub/MainActivity.kt
+++ app/src/main/java/com/example/devhub/MainActivity.kt
@@ -1,4 +1,26 @@
-package br.com.alura.devhub
+package com.example.devhub
+
+import android.os.Bundle
+import android.view.Surface
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import com.example.devhub.ui.theme.DevHubTheme
+
+//package br.com.alura.devhub
 
 // imports
 
@@ -10,9 +32,15 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Column {
+                        Image(
+                            painter = painterResource(id = R.drawable.i149025766),
+                            contentDescription = "My Image"
+                        )
+                        Text(text = "This is my image")
+                    }
                 }
             }
         }
