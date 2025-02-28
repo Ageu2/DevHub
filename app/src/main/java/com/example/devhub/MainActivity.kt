ef2bcc5c6bbd95b4e519e2fdc23e2333f8efa26a
Ageu <acasil77@gmail.com>
2025-02-28 14:21:13 -0400

    day2 finish, center image,format background and text

--- app/src/main/java/com/example/devhub/MainActivity.kt
+++ app/src/main/java/com/example/devhub/MainActivity.kt
@@ -1,22 +1,38 @@
 package com.example.devhub
 
 import android.os.Bundle
+import android.util.EventLogTags.Description
 import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ReportFragment.Companion.reportFragment
 import com.example.devhub.ui.theme.DevHubTheme
 
@@ -32,19 +48,65 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column {
-                        Image(
-                            painter = painterResource(id = R.drawable.i149025766),
-                            contentDescription = "My Image"
-                        )
-                        Text(text = "Ageu")
-                        Text(text = "Ageu2")
-                        Text(text = "DeveloperStudent")
-                    }
+                    color = MaterialTheme.colorScheme.background)
+                {
+                    Layoutcolor()//order positio for see
+                    ProfileScreen()
+
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ProfileScreen() {
+    Column (
+        modifier=Modifier
+            .fillMaxSize()
+            .padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.i149025766),
+            contentDescription = "My Image",
+            modifier = Modifier.run {
+                size(150.dp)
+                        .padding(10.dp)
+                        .clip(CircleShape)//format image in circle
+            }
+
+        )
+
+        //with color in text
+        Text(text = "Ageu", color = Color.Blue)
+        Text(text = "Ageu2", color = Color.Blue)
+        Text(text = "DeveloperStudent", color = Color.Blue)
+
+    }
+}
+
+@Composable
+fun Layoutcolor() {
+    Column (modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .height(100.dp)
+                .fillMaxSize()
+                .background(Color.DarkGray, shape = RoundedCornerShape(5.dp))
+
+
+        )
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize()
+                .background(Color.LightGray, shape = RoundedCornerShape(5.dp))
+
+
+        )
+
     }
 }
