26edfac28be322fef52934a1e9f41d063c43dd4a
Ageu <acasil77@gmail.com>
2025-03-01 21:46:44 -0400

    day 3 insert image from url

--- app/src/main/java/com/example/devhub/MainActivity.kt
+++ app/src/main/java/com/example/devhub/MainActivity.kt
@@ -22,9 +22,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+
 import coil.compose.rememberAsyncImagePainter
 import com.example.devhub.ui.theme.DevHubTheme
 //package br.com.alura.devhub
