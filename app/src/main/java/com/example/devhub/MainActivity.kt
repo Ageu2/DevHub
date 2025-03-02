d564aeb08d4ebe6ab5d69b548592bd4c3d6f57c1
Ageu <acasil77@gmail.com>
2025-03-01 21:46:00 -0400

    day 3 insert image from url

--- app/src/main/java/com/example/devhub/MainActivity.kt
+++ app/src/main/java/com/example/devhub/MainActivity.kt
@@ -1,19 +1,12 @@
 package com.example.devhub
 
 import android.os.Bundle
-import android.util.EventLogTags.Description
-import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -21,7 +14,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,12 +22,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import com.example.devhub.ui.theme.DevHubTheme
-
 //package br.com.alura.devhub
 
 // imports
@@ -48,10 +39,12 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background)
+                    color = MaterialTheme.colorScheme.background
+                )
                 {
-                    Layoutcolor()//order positio for see
+                    Layoutcolor() //order positio for see
                     ProfileScreen()
+                    //ImageFromUrl(imageUrl = "https://avatars.githubusercontent.com/u/149025766?v=4")
 
                 }
             }
@@ -67,28 +60,38 @@ fun ProfileScreen() {
             .padding(30.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-
-        Image(
+        //insert image with archive
+        /*Image(
             painter = painterResource(id = R.drawable.i149025766),
             contentDescription = "My Image",
-            modifier = Modifier.run {
-                size(150.dp)
-                        .padding(10.dp)
-                        .clip(CircleShape)//format image in circle
-            }
+            modifier = Modifier
+                .size(150.dp)
+                .padding(10.dp)
+                .clip(CircleShape)//format image in circle
 
+        )*/
+        //  inser image view with url
+        val painter = rememberAsyncImagePainter(
+            model = "https://avatars.githubusercontent.com/u/149025766?v=4",
+            placeholder = painterResource(id = R.drawable.ic_launcher_foreground),
+            error = painterResource(id = R.drawable.ic_launcher_foreground)
         )
-
+        Image(
+            painter = painter,
+            contentDescription = "Image from URL",
+            modifier = Modifier
+                .size(150.dp)
+                .padding(10.dp)
+                .clip(CircleShape)//format image in circle
+        )
         //with color in text
         Text(text = "Ageu", color = Color.Blue)
         Text(text = "Ageu2", color = Color.Blue)
         Text(text = "DeveloperStudent", color = Color.Blue)
-
     }
 }
-
 @Composable
-fun Layoutcolor() {
+fun Layoutcolor () {
     Column (modifier = Modifier.fillMaxSize()) {
         Box(
             modifier = Modifier
